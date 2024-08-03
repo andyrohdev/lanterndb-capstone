30e061b610a63cd15323974de055ee2df5907755
conner eberhardt <ceberhardt1005@gmail.com>
2024-08-03 10:58:06 -0400

    pushing

--- java/src/main/java/com/techelevator/controller/GameController.java
+++ java/src/main/java/com/techelevator/controller/GameController.java
@@ -2,18 +2,20 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.GamesDao;
 import com.techelevator.model.Games;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
 import java.security.Principal;
 import java.util.List;
-
+@CrossOrigin
 @RestController
 public class GameController {
-
+private String API_URL = "http://localhost:8080/games";
+private String API_KEY = "916f7023c320440daebd11eb40e66e2e";
+private RestTemplate restTemplate;
 private GamesDao gamesDao;
+
 
 
     @RequestMapping(path="/games", method = RequestMethod.GET)
@@ -22,4 +24,12 @@ private GamesDao gamesDao;
         return fetchedGames;
     }
 
-}
+//    @GetMapping(value = "/clientcallgames")
+//    public List<Games> retrieveAllGames(){
+//        String uri = API_URL;
+//        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+//        return result;
+//    }
+
+    }
