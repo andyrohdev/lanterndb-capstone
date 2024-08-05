774d3c4656b28b844bb08a94193c3fd0be6207de
Steven Turner <stevenjturner013@gmail.com>
2024-08-05 10:50:17 -0400

    fixed sql scripts and put them into source scripts to run from create.sh

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,5 +2,8 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO collections (collection_name) VALUES ('Wish List'), ('Currently Playing'), ('Played');
+INSERT INTO collection_list (user_id, collection_id, title, genre) VALUES (3, 1, 'Border Lands', 'Looter Shooter');
+
 
 COMMIT TRANSACTION;
