2ca2edeaef46a2cc210141bb3b4b0b2e4f3934e5
alexis zabsonre <alexzabs4@gmail.com>
2024-08-06 14:05:10 -0400

    saving progress

--- vue/src/components/Card.vue
+++ vue/src/components/Card.vue
@@ -3,7 +3,7 @@
     <h3>{{ title }}</h3>
     <ul>
       <li v-for="item in items" :key="item.collectionListId">
-        {{ item.collection_list_id }}
+        
         {{ item.title }}
         <div class="dropdown">
           <button
