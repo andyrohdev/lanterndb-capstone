329c7918991fbca83e03b300a23e5b594d5c9e66
Steven Turner <stevenjturner013@gmail.com>
2024-08-11 13:34:26 -0400

    added server side functionality for updating user ratings/getting ratings for user profiles

--- java/src/main/java/com/techelevator/controller/RatingController.java
+++ java/src/main/java/com/techelevator/controller/RatingController.java
@@ -1,14 +1,18 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.JdbcRatingDao;
+import com.techelevator.dao.JdbcUserDao;
 import com.techelevator.dao.RatingDao;
 import com.techelevator.model.CollectionList;
 import com.techelevator.model.Rating;
+import com.techelevator.model.Review;
 import com.techelevator.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -17,6 +21,10 @@ import java.util.List;
 public class RatingController {
     @Autowired
     private RatingDao ratingDao;
+    @Autowired
+    private JdbcUserDao jdbcUserDao;
+    @Autowired
+    private JdbcRatingDao jdbcRatingDao;
 
     @GetMapping("/rating")
     public Rating getRatingById(@RequestBody Rating rating) {
@@ -36,27 +44,24 @@ public class RatingController {
         return foundRatings;
     }
 
-//    @GetMapping("/collections/{id}")
-//    public List<CollectionList> fetchCollectionListById(
-//            Principal principal,
-//            @PathVariable int id){
-//
-//        if (principal == null) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not authenticated");
-//        }
-//
-//        String userName = principal.getName(); // Get the username from the Principal
-//        User user = jdbcUserDao.getUserByUsername(userName);
-//        int userId = user.getId();
-//
-//        List<CollectionList> foundCollection = jdbcCollectionListDao.fetchCollectionByType(id, userId);
-//
-//        if (foundCollection == null) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Collection not found");
-//        }
-//
-//        return foundCollection;
-//    }
+    @PutMapping("/user/ratings")
+    public List<Rating> updateRating(@Valid @RequestBody Rating rating, Principal principal) {
+        String userName = principal.getName();
+        User user = jdbcUserDao.getUserByUsername(userName);
+        int userId = user.getId();
+
+        rating.setUser_id(userId);
+
+        return ratingDao.updateRatingById(rating);
+
+    }
+
+    @GetMapping("/user/ratings/{id}")
+    public List <Rating> getProfileRatings(@PathVariable int id) {
+        List<Rating> ratings = jdbcRatingDao.fetchProfileRatings(id);
+        return ratings;
+    }
+
 
     @PostMapping("/rating")
     public Rating addRating(@RequestBody Rating rating) {
