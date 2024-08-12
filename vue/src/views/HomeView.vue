26f8dd244d600e6434bc40ab6327c693d14327f0
alexis zabsonre <alexzabs4@gmail.com>
2024-08-12 11:32:58 -0400

    Redirect on Home Page working

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,67 +2,58 @@
   <div class="home-page">
     <header class="header-section">
       <div class="header-section-one">
-        <h1>
-          Welcome to LanternDb
-        </h1>
-        <h2>
-          Illuminate Your Gaming World
-        </h2>
-      <p>
-        Step into LanternDB, the ultimate database for gaming enthusiasts. 
-        Discover detailed information on more than 800,000 video games, from beloved classics to cutting-edge new releases. 
-        Dive into reviews, ratings, and in-depth articles that bring your favorite games to life.
-      </p>
-      <p>
-        Whether you're searching for a hidden gem or staying updated on the latest trends,
-        LanternDB is your go-to resource for everything gaming
-      </p>
-
+        <h1>Welcome to LanternDb</h1>
+        <h2>Illuminate Your Gaming World</h2>
+        <p>
+          Step into LanternDB, the ultimate database for gaming enthusiasts.
+          Discover detailed information on more than 800,000 video games, from
+          beloved classics to cutting-edge new releases. Dive into reviews,
+          ratings, and in-depth articles that bring your favorite games to
+          life.
+        </p>
+        <p>
+          Whether you're searching for a hidden gem or staying updated on the
+          latest trends, LanternDB is your go-to resource for everything gaming.
+        </p>
       </div>
     </header>
-    <div class="featured-games">
-    <!-- <div v-for="game in featuredGames" :key="game.id" class="game-item">
-      <router-link :to="{ name: 'GameDetails', params: { id: game.id } }">
-        <img :src="game.image" :alt="game.title" />
-        <h3>{{ game.title }}</h3>
-      </router-link>
-    </div> -->
     <div class="featured-games-section">
       <h2>Featured Games: </h2>
       <div class="games-container">
-        <!-- <div 
+        <div 
           v-for="game in featuredGames" 
           :key="game.id" 
           class="game-card"
           @click="redirectToGameDetails(game.id)"
         >
-          <img :src="game.imageUrl" :alt="game.title" class="game-image" />
-          <p>{{ game.title }}</p>
-        </div> -->
+          <img :src="game.imageUrl" :alt="game.name" class="game-image" />
+          <p>{{ game.name }}</p>
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
       featuredGames: [
-        { id: 1, name: "GTA", imageUrl: "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg" },
-        { id: 2, name: "PTwo", imageUrl: "	https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg" },
-        { id: 3, name: "TWThree", imageUrl: "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg" },
-        { id: 4, name: "Game Four", imageUrl: "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg" },
+        { id: 3498, name: "GTA", imageUrl: "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg" },
+        { id: 4200, name: "PTwo", imageUrl: "https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg" },
+        { id: 3328, name: "TWThree", imageUrl: "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg" },
+        { id: 4291, name: "CSGO", imageUrl: "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg" },
       ],
     };
   },
   methods: {
-    // redirectToGameDetails(gameId) {
-    //   // this.$router.push({ name: 'gameDetails', params: { id: gameId } });
-    // },
+    redirectToGameDetails(gameId) {
+      this.$router.push({ name: 'game-details', params: { gameId: gameId } });
+    },
   },
 };
 </script>
+
 <style scoped>
 .home-page {
   margin: 0;
