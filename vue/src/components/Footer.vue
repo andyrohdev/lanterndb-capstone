79cd4c27510a549bd9a6cb0ffb7946e95ef47b19
conner eberhardt <ceberhardt1005@gmail.com>
2024-08-13 14:28:35 -0400

    pull for updated code

--- vue/src/components/Footer.vue
+++ vue/src/components/Footer.vue
@@ -1,33 +1,35 @@
 <template>
     <footer class="custom-footer">
-    <ul class="nav-list">
-      <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-    </ul>
-    <p class="footer-text">© 2024 Company, Inc</p>
-  </footer>
+        <ul class="nav-list">
+            <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+        <p class="footer-text">© 2024 Company, Inc</p>
+    </footer>
 </template>
 
 <script>
-    export default{
-        name: 'Footer',
-    };
+export default {
+    name: 'Footer',
+};
 </script>
     
 <style scoped>
-.custom-footer{
-padding-top: 1rem;
-padding-bottom: 1.5rem;
-text-align: center;
-background-color: rgb(0, 0, 0);;
-z-index: 100;
-position: relative;
-min-height: 100px;
+.custom-footer {
+    padding-top: 1rem;
+    padding-bottom: 1.5rem;
+    text-align: center;
+    background-color: rgb(0, 0, 0);
+    ;
+    z-index: 100;
+    position: relative;
+    min-height: 100px;
 }
-.nav-list{
+
+.nav-list {
     list-style-type: none;
     padding: 0;
     margin: 0;
@@ -37,21 +39,21 @@ min-height: 100px;
     padding-bottom: 1rem;
     margin-bottom: 1rem;
 }
-.nav-link{
+
+.nav-link {
     text-decoration: none;
     color: rgb(255, 255, 255);
     padding-left: .5rem;
     padding-right: .5rem;
     font-size: 1rem;
 }
-.nav-link:hover{
+
+.nav-link:hover {
     color: orange;
     text-decoration: underline;
-    
+
 }
-.footer-text{
+
+.footer-text {
     color: white;
-}
-
-
-</style>
+}</style>
